c0c8f6d1a65763a44b816fe5711320ed8e02e8cf
Ryou-Watanabe <robotechnovate@gmail.com>
2016-12-29 15:30:31 +0000

    added timer

--- led_fire/led_fire.c
+++ led_fire/led_fire.c
@@ -4,16 +4,46 @@
 #include <linux/device.h> 
 #include <asm/uaccess.h>
 #include <linux/io.h>
+#include <linux/kthread.h>
+#include <linux/sched.h>
+#include <linux/jiffies.h>
 
 MODULE_AUTHOR("Ryou Watanabe");
 MODULE_DESCRIPTION("driver for LED control");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.1");
 
+double sec = 1.0;
+int flag=0;
 static dev_t dev;
 static struct cdev cdv;
 static struct class *cls = NULL;
 static volatile u32 *gpio_base = NULL;
+
+static struct task_struct *kthread_tsk;
+static void my_kthread_main(void)
+{
+       set_current_state(TASK_INTERRUPTIBLE);
+       //schedule_timeout(0.1*HZ);
+	   if(flag == 0){
+           schedule_timeout(0.1*HZ);
+		   flag = 1;
+           gpio_base[10] = 1 << 25;      
+	   }else if(flag == 1){
+       	   schedule_timeout(1.0*HZ);
+		   flag = 0;
+           gpio_base[7] = 1 << 25;      
+	   }
+	   printk("kthread:%ld\n", jiffies);
+}
+static int my_kthread(void *arg)
+{
+       printk("HZ:%d\n", HZ);
+       while (!kthread_should_stop()) {
+            my_kthread_main();
+        }
+       return 0;
+}
 /*
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
@@ -22,18 +52,37 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 }
 */
 
+
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
+	int i;
 	char c;
     if(copy_from_user(&c,buf,sizeof(char)))
 	    return -EFAULT;
-
-	if(c == '0')
-		gpio_base[10] = 1 << 25;
-	else if(c == '1')
-		gpio_base[7] = 1 << 25;
 	
-	printk(KERN_INFO "receive %c\n",c);
+
+	if(c == '0'){
+		gpio_base[10] = 1 << 25;
+		printk(KERN_INFO "\e[31mLED OFF\e[m\n");
+//		sec = 2;
+	}else if(c == '1'){
+		gpio_base[7] = 1 << 25;
+		printk(KERN_INFO "\e[31mLED ON\e[m\n");
+//		sec = 1;
+	}else if(c == 'w'){
+		for(i=0;i<5;i++){
+		    printk(KERN_INFO "%d\n",i);
+            set_current_state(TASK_INTERRUPTIBLE);
+       	    schedule_timeout(i*HZ);
+		}
+	}/*else if(c == 'l'){
+		for(i=0;i<100;){
+            set_current_state(TASK_INTERRUPTIBLE);
+       	    schedule_timeout(i*HZ);
+			i=i+0.1;
+		}
+	}*/
+	printk(KERN_INFO "receive \e[31m%c\e[m\n",c);
 	return 1;
 }
 
@@ -83,6 +132,14 @@ static int __init init_mod(void)
 		printk(KERN_ERR "cdev_add failed. major:%d, minor:%d",MAJOR(dev),MINOR(dev));
         return retval;
 	}
+    kthread_tsk = kthread_run(my_kthread, NULL, "param %s&%d", "test", 123);
+    if (IS_ERR(kthread_tsk))
+        return -1;
+    printk("pid->%d:prio->%d:comm->%s\n",
+        kthread_tsk->pid,
+        kthread_tsk->static_prio,
+		kthread_tsk->comm);
+	return 0;
 
 	cls = class_create(THIS_MODULE,"led_fire");
 	if(IS_ERR(cls)){
@@ -98,6 +155,7 @@ static void __exit cleanup_mod(void)
 	cdev_del(&cdv);
 	printk(KERN_INFO "%s is unloaded.\n",__FILE__);
 	unregister_chrdev_region(dev, 1);
+    kthread_stop(kthread_tsk);
 	printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
     device_destroy(cls, dev);
     class_destroy(cls);
